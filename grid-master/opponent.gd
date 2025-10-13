d547152839be32a3a7fd9d548d4c766556a93dc6
uchy5 <131214562+uchy5@users.noreply.github.com>
2025-10-13 16:17:37 -0600

    Grid master (#18)
    * GridMaster connection
    
    * added grid.gd
    
    * Should be updated to version 4.5 now
    
    * grid
    
    i made a grid system
    
    * Update icon.svg.import
    
    * Base Project
    
    Create a new project test
    
    * Grid
    
    Basic grid is back and working
    
    * health
    
    health bar
    
    * Readded folders
    
    Should be both folders and hp bar
    
    * FIle images changes
    
    Extra files images create need to be existing
    
    * Fix scene
    
    Changed to scene that the health bar was made on
    
    ---------
    
    Co-authored-by: Justshmee <dylrawlings@gmail.com>
    Co-authored-by: Johnathon Anderson <and21125@byui.edu>
    Co-authored-by: Wurmz57 <jaden.mons@hotmail.com>

---
+++ grid-master/opponent.gd
@@ -0,0 +1,7 @@
+extends Node
+#Health
+class_name Opponenet
+
+
+var Max_health = 100
+var Current_health = 100 
---
+++ grid-master/player.gd
@@ -0,0 +1,8 @@
+extends Node
+#Health
+class_name Player
+signal healthChanged
+
+
+var Max_health = 100
+var Current_health = 100 
