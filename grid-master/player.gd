dccbe9b7a92408ee70a85bd927131cdbbe1747ee
Kaleb <magnetar03@gmail.com>
2025-10-15 16:16:58 -0600

    Grid master (#21)
    * GridMaster connection
    
    * added grid.gd
    
    * Should be updated to version 4.5 now
    
    * grid
    
    i made a grid system
    
    * Update icon.svg.import
    
    * Base Project
    
    Create a new project test
    
    * Grid
    
    Basic grid is back and working
    
    * health
    
    health bar
    
    * Readded folders
    
    Should be both folders and hp bar
    
    * FIle images changes
    
    Extra files images create need to be existing
    
    * Fix scene
    
    Changed to scene that the health bar was made on
    
    * worked on health bar
    
    I was able to work on the health and damge, allowing for the bar to be updated and show what the health is at, SPACE does 10 dmg and H does 10 hp+
    
    * Player Scene Fix & card added to main scene again
    
    ---------
    
    Co-authored-by: Justshmee <dylrawlings@gmail.com>
    Co-authored-by: Johnathon Anderson <and21125@byui.edu>
    Co-authored-by: Wurmz57 <jaden.mons@hotmail.com>
    Co-authored-by: uchy5 <131214562+uchy5@users.noreply.github.com>

--- grid-master/player.gd
+++ grid-master/player.gd
@@ -1,8 +1,27 @@
 extends Node
-#Health
 class_name Player
-signal healthChanged
 
+var max_health := 100
+var health := max_health
 
-var Max_health = 100
-var Current_health = 100 
+@onready var health_bar = $CanvasLayer/HealthBar
+
+func _ready():
+	_update_health()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):  # Space key
+		apply_damage(10)
+	elif Input.is_action_just_pressed("heal"):     # H key
+		heal(10)
+
+func apply_damage(amount: int):
+	health = clamp(health - amount, 0, max_health)
+	_update_health()
+
+func heal(amount: int):
+	health = clamp(health + amount, 0, max_health)
+	_update_health()
+
+func _update_health():
+	health_bar.update_health(health, max_health)
