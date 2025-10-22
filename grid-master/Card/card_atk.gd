eecb81ce868052fd3df9081457b7da01d18e69d5
97Kaleb <magnetar03@gmail.com>
2025-10-22 15:49:43 -0600

    Fixed card_atk scene inheritance

--- grid-master/Card/card_atk.gd
+++ grid-master/Card/card_atk.gd
@@ -5,7 +5,7 @@ func effect_one():
 	print("Move Action")
 
 func effect_two():
-	PlayerAddValue.emit_signal("player_add_value", 1, 2, 10)
+	PlayerAddValue.emit_signal("player_add_value", 1, 1, -10)
 
 func effect_three():
 	PlayerAddValue.emit_signal("player_add_value", 1, 3, -2)
