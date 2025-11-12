34a828e5aa4fce27db69b2a11578a22c7d332a51
97Kaleb <magnetar03@gmail.com>
2025-11-12 15:56:00 -0700

    Updated signal
    objectID rather than playerID. objectID 1 and 2 are player1 and player2, respectively.

--- grid-master/GlobalSignals.gd
+++ grid-master/GlobalSignals.gd
@@ -4,5 +4,7 @@ extends Node
 signal player_add_value(playerID, valueID, value)
 @warning_ignore("unused_signal")
 signal player_move(playerID, range)
+
 @warning_ignore("unused_signal")
-signal free_move(playerID, X, Y)
+signal free_move(objectID, X, Y)
+# ObjectIDs: 1: player1, 2: player2, (add other objects' IDs as they're implemented.)
