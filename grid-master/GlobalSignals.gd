080a19527a6d28facb2fc109911c2ef9ee05e9d5
97Kaleb <magnetar03@gmail.com>
2025-11-14 15:39:53 -0700

    Updated comments for GlobalSignals

--- grid-master/GlobalSignals.gd
+++ grid-master/GlobalSignals.gd
@@ -4,11 +4,9 @@ extends Node
 signal player_add_value(playerID, valueID, value)
 @warning_ignore("unused_signal")
 signal player_move(playerID, range)
-<<<<<<< Updated upstream
 
-=======
-#Range will be the tile set it pulls from to find valid move squares
->>>>>>> Stashed changes
 @warning_ignore("unused_signal")
 signal free_move(objectID, X, Y)
-# ObjectIDs: 1: player1, 2: player2, (add other objects' IDs as they're implemented.)
+# ObjectIDs: 1: player1, 2: player2, 3: Effect Zone (damage, healing, status, 
+# etc), 10: Neutral Spawnable, 11: team1 Spawnable, 12: team2 Spawnable
+# (add other objects' IDs as they're implemented.)
